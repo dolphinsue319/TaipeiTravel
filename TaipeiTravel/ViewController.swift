fe7087be008fd36827577d684f4ad468a59707b3
Kedia <howardsue319@gmail.com>
2024-12-28 08:24:57 +0800

    Initial Commit

---
+++ TaipeiTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaipeiTravel
+//
+//  Created by Kedia on 2024/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
