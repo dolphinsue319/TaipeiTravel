e9712f479bbf8f00e0d4bb3e89029b441f5cb178
Kedia <howardsue319@gmail.com>
2024-12-29 16:41:40 +0800

    adds TTLocazation.swift

---
+++ TaipeiTravel/TTLocalization.swift
@@ -0,0 +1,20 @@
+//
+//  TTLocalization.swift
+//  TaipeiTravel
+//
+//  Created by Kedia on 2024/12/29.
+//
+
+import Foundation
+
+enum TTLocalization {
+    case zhTw
+    case zhCn
+    case en
+    case ja
+    case ko
+    case es
+    case id
+    case th
+    case vi
+}
